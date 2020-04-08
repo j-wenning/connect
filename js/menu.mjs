24f37292cc8094680faac6eadaafc897a9fa8276
justin-wenning <wenning.justin@gmail.com>
2020-04-08 14:26:33 -0700

    Added functionality to menu's update method Updated menu's render method to display default values Updated menu's render method to display the proper possible player count

--- js/menu.mjs
+++ js/menu.mjs
@@ -9,6 +9,7 @@ class Menu {
     this.maxTime = maxTime;
     this.winCon = winCon;
     this.render();
+    this.update(state);
   }
 
   toggle() {
@@ -16,7 +17,35 @@ class Menu {
     cur.classList.toggle('closed');
   }
 
-  update() {}
+  update(state) {
+    const { boardX, boardY, scores, maxTime, winCon } = state;
+    let cur;
+    if (this.boardX !== boardX) {
+      cur = document.querySelector('#boardX');
+      cur.value = boardX;
+      this.boardX = boardX;
+    }
+    if (this.boardY !== boardY) {
+      cur = document.querySelector('#boardY');
+      cur.value = boardY;
+      this.boardY = boardY;
+    }
+    if (this.playerCount !== scores.length) {
+      cur = document.querySelector('#players');
+      cur.value = scores.length;
+      this.playerCount = scores.length;
+    }
+    if (this.maxTime !== maxTime) {
+      cur = document.querySelector('#timeLimit');
+      cur.value = maxTime;
+      this.maxTime = maxTime;
+    }
+    if (this.winCon !== winCon) {
+      cur = document.querySelector('#winCon');
+      cur.value = winCon;
+      this.winCon = winCon;
+    }
+  }
 
   render() {
     let cur = document.querySelector('#menu');
@@ -55,15 +84,15 @@ class Menu {
     cur = cur.appendChild(document.createElement('form'));
     [
       { elem: 'label', for: 'boardX', textContent: 'x' },
-      { elem: 'input', id: 'boardX', type: 'text' },
+      { elem: 'input', id: 'boardX', type: 'number', value: this.boardX },
       { elem: 'label', for: 'boardY', textContent: 'y' },
-      { elem: 'input', id: 'boardY', type: 'text' },
+      { elem: 'input', id: 'boardY', type: 'number', value: this.boardY },
       { elem: 'label', for: 'players', textContent: 'player count' },
-      { elem: 'select', id: 'players', val: 2 },
+      { elem: 'select', id: 'players' },
       { elem: 'label', for: 'timeLimit', textContent: 'time limit' },
-      { elem: 'input', id: 'timeLimit', type: 'number' },
+      { elem: 'input', id: 'timeLimit', type: 'number', value: Number(this.maxTime) },
       { elem: 'label', for: 'winCon', textContent: 'win condition' },
-      { elem: 'input', id: 'winCon', type: 'number'},
+      { elem: 'input', id: 'winCon', type: 'number', value: this.winCon },
       { elem: 'button', id: 'reset', type: 'reset', textContent: 'reset' },
       { elem: 'button', id: 'create',type: 'submit', textContent: 'submit' }
     ].forEach(item => {
@@ -73,6 +102,13 @@ class Menu {
         else if (key !== 'elem') cur.setAttribute(key, item[key]);
       }
       cur = cur.parentElement;
-    })
+    });
+    cur = document.querySelector('#players');
+    // eslint-disable-next-line no-undef
+    for (let i = 2; i < TOKENS.length; ++i) {
+      cur = cur.appendChild(document.createElement('option'));
+      cur.value = cur.textContent = i;
+      cur = cur.parentElement;
+    }
   }
 }
